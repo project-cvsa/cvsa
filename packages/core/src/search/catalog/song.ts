7d3d45e5ca49bc301468e7b2430f946d88c12d7c
alikia2x <alikia2x@outlook.com>
2026-04-30 17:15:27 +0800

    refactor: extract common methods in search services

--- packages/core/src/search/catalog/song.ts
+++ packages/core/src/search/catalog/song.ts
@@ -1,8 +1,6 @@
 import type { SongType } from "@cvsa/db";
 import { ISearchService } from "../interface";
-import { unique, keys } from "remeda";
 import type { SongDetailsResponseDto } from "../../modules";
-import { appLogger } from "@cvsa/logger";
 
 export interface SongSearchIndex {
 	id: number;
@@ -23,19 +21,20 @@ export interface SongSearchIndex {
 	};
 }
 
-export class SongSearchService extends ISearchService<SongDetailsResponseDto> {
-	private async getDocument(
+export class SongSearchService extends ISearchService<
+	SongDetailsResponseDto,
+	SongSearchIndex
+> {
+	protected readonly entityType = "song";
+
+	protected async getDocument(
 		song: SongDetailsResponseDto,
-		language: string
+		language: string,
 	): Promise<SongSearchIndex> {
-		const getDesc = () => {
-			if (language === song.language) return song.description;
-			return song.localizedDescriptions?.[language];
-		};
 		const getLyrics = () => {
 			return song.lyrics.find((item) => item.language === language)?.plainText;
 		};
-		const getLocalizedName = (attr: "singers" | "artists") => {
+		const getLocalizedRelatives = (attr: "singers" | "artists") => {
 			const data = song[attr].map((item) => {
 				if (item.language === language) {
 					return item.name;
@@ -45,20 +44,17 @@ export class SongSearchService extends ISearchService<SongDetailsResponseDto> {
 			return data.filter(Boolean) as string[];
 		};
 		const getSingers = () => {
-			return getLocalizedName("singers");
+			return getLocalizedRelatives("singers");
 		};
 		const getArtists = () => {
-			return getLocalizedName("artists");
+			return getLocalizedRelatives("artists");
 		};
-		const getName = () => {
-			if (language === song.language) return song.name;
-			return song.localizedNames?.[language];
-		};
+
 		const vectors = await this.embeddingManager.embeddings.post({
 			texts: [
-				`Name: ${getName() ?? ""}
-Lyrics: ${song.lyrics ?? ""}
-Description: ${getDesc() ?? ""}
+				`Name: ${this.getName(song, language) ?? ""}
+Lyrics: ${getLyrics() ?? ""}
+Description: ${this.getDescription(song, language) ?? ""}
 Singers: ${getSingers().join(", ")}
 Artists: ${getArtists().join(", ")}
 `,
@@ -66,9 +62,9 @@ Artists: ${getArtists().join(", ")}
 		});
 		return {
 			id: song.id,
-			name: getName() ?? undefined,
+			name: this.getName(song, language) ?? undefined,
 			lyrics: getLyrics() ?? undefined,
-			description: getDesc() ?? undefined,
+			description: this.getDescription(song, language) ?? undefined,
 			singers: getSingers(),
 			artists: getArtists(),
 			bilibiliAid: song.bilibiliAid ?? undefined,
@@ -77,69 +73,10 @@ Artists: ${getArtists().join(", ")}
 			engine: song.singers
 				.map((item) => item.engine ?? undefined)
 				.filter(Boolean) as string[],
-			publishedAt: song.publishedAt ? new Date(song.publishedAt).getTime() : undefined,
-			_vectors: vectors.data?.embeddings[0]
-				? {
-						"potion-multilingual-128M": vectors.data?.embeddings[0],
-					}
-				: {
-						"potion-multilingual-128M": null,
-					},
-		};
-	}
-
-	public async sync(id: number) {
-		if (!this.searchManager) {
-			appLogger.warn("Search service not available");
-			return;
-		}
-		const song = await this.repository.getDetailsById(id);
-
-		if (!song) {
-			const indexesToBeDeleted = await this.searchManager.getLocalizedIndexesOfEntity("song");
-			for (const index of indexesToBeDeleted) {
-				const adminIndex = await this.searchManager.getAdminIndex(index);
-				const task = await adminIndex.deleteDocument(id);
-				await this.searchManager.waitForTask(task.taskUid);
-			}
-			return;
-		}
-
-		const languages = unique([
-			...keys(song.localizedNames ?? []),
-			...keys(song.localizedDescriptions ?? []),
-			song.language,
-		]);
-		for (const language of languages) {
-			const indexUid = `song_${language}`;
-			const index = await this.searchManager.getAdminIndex<SongSearchIndex>(indexUid);
-			const document = await this.getDocument(song, language);
-			const task = await index.addDocuments([document], {
-				primaryKey: "id",
-			});
-			await this.searchManager.waitForTask(task.taskUid);
-		}
-	}
-
-	public async search(query: string, language: string = "zh") {
-		if (!this.searchManager) {
-			throw new Error("Search or embedding service not available");
-		}
-
-		const index = await this.searchManager.getSearchIndex(`song_${language}`);
-		const embeddingResponse = await this.embeddingManager.embeddings.post({
-			texts: [query],
-		});
-		const embeddingAvailable = (embeddingResponse?.data?.embeddings[0]?.length ?? 0) > 0;
-		return index.search(query, {
-			vector: embeddingResponse?.data?.embeddings[0],
-			hybrid: embeddingAvailable
-				? {
-						embedder: "potion-multilingual-128M",
-						semanticRatio: 0.25,
-					}
+			publishedAt: song.publishedAt
+				? new Date(song.publishedAt).getTime()
 				: undefined,
-			showRankingScore: true,
-		});
+			_vectors: this.buildVectors(vectors.data?.embeddings[0]),
+		};
 	}
 }
